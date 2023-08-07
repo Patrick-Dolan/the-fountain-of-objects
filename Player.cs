aebc8f1475598e5da6922eba371e03cb42e8f727
Patrick Dolan <dolanp1992@gmail.com>
2023-08-07 10:55:05 -0700

    add Name, CurrentLocation, and Constructor

---
+++ Player.cs
@@ -0,0 +1,13 @@
+﻿namespace TheFountainOfObjects
+{
+    internal class Player
+    {
+        public string Name { get; set; } = "Player One";
+        public int[] CurrentLocation { get; set; } = new int[2] { 0, 0 };
+        public Player() { }
+        public Player(string name)
+        {
+            Name = name;
+        }
+    }
+}
