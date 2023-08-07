6d0d6c8716b12e85dd9aaaccdef969d0fc038fe5
Patrick Dolan <dolanp1992@gmail.com>
2023-08-07 13:05:16 -0700

    add props and constructor

---
+++ GameManager.cs
@@ -0,0 +1,14 @@
+﻿namespace TheFountainOfObjects
+{
+    internal class GameManager
+    {
+        public Player Player { get; set; } = new Player();
+        public Map Map { get; } = new Map();
+        public bool Winner { get; set; } = false;
+        public bool FountainEnabled { get; set; } = false;
+        public GameManager(Player player)
+        {
+            Player = player;
+        }
+    }
+}
