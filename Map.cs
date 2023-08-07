1ceb576e264535d354d321fcd42c46575e8730f8
Patrick Dolan <dolanp1992@gmail.com>
2023-08-07 09:07:53 -0700

    Add project files.

---
+++ Map.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheFountainOfObjects
+{
+    internal class Map
+    {
+        public string[,] MapLocations { get; set; } = new string[4, 4];
+        public Map() { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using TheFountainOfObjects;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("The Fountain of Objects");
+        Console.WriteLine("=======================");
+
+        Map gameMap = new Map();
+    }
+}
