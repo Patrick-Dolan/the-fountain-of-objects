1802120e1bbdc238c2c814f0b2995b95d5037c79
Patrick Dolan <dolanp1992@gmail.com>
2023-08-08 08:08:35 -0700

    add PlayerHasWon, ToggleFountain, and MovePlayer methods

--- GameManager.cs
+++ GameManager.cs
@@ -10,5 +10,32 @@
         {
             Player = player;
         }
+        public bool PlayerHasWon() => (FountainEnabled && Player.CurrentLocation[0] == 0 && Player.CurrentLocation[1] == 0);
+        public void ToggleFountain() => FountainEnabled = !FountainEnabled;
+        public void MovePlayer(string direction)
+        {
+            switch (direction)
+            {
+                case "move north":
+                    if (Player.CurrentLocation[1] - 1 < Map.MinIndex) return;
+                    Player.CurrentLocation[1]--;
+                    break;
+                case "move south":
+                    if (Player.CurrentLocation[1] + 1 > Map.MaxIndex) return;
+                    Player.CurrentLocation[1]++;
+                    break;
+                case "move east":
+                    if (Player.CurrentLocation[0] - 1 < Map.MinIndex) return;
+                    Player.CurrentLocation[0]++;
+                    break;
+                case "move west":
+                    if (Player.CurrentLocation[0] + 1 > Map.MaxIndex) return;
+                    Player.CurrentLocation[0]--;
+                    break;
+                default:
+                    Console.WriteLine("Error: GameManager MovePlayer method invalid input.");
+                    break;
+            }
+        }
     }
 }
