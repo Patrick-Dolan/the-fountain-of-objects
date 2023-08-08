5de9832eb9fcb6c02b2284d79a14f1f7697ecaac
Patrick Dolan <dolanp1992@gmail.com>
2023-08-08 09:40:06 -0700

    fix MovePlayer boundary detection, remove ToggleFountain method, add ValidCommands array

--- GameManager.cs
+++ GameManager.cs
@@ -6,12 +6,9 @@
         public Map Map { get; } = new Map();
         public bool Winner { get; set; } = false;
         public bool FountainEnabled { get; set; } = false;
-        public GameManager(Player player)
-        {
-            Player = player;
-        }
+        public string[] ValidCommands { get; set; } = new string[] { "enable fountain", "move north", "move east", "move south", "move west" };
+        public GameManager() { }
         public bool PlayerHasWon() => (FountainEnabled && Player.CurrentLocation[0] == 0 && Player.CurrentLocation[1] == 0);
-        public void ToggleFountain() => FountainEnabled = !FountainEnabled;
         public void MovePlayer(string direction)
         {
             if (direction == null) return;
@@ -26,11 +23,11 @@
                     Player.CurrentLocation[1]++;
                     break;
                 case "move east":
-                    if (Player.CurrentLocation[0] - 1 < Map.MinIndex) return;
+                    if (Player.CurrentLocation[0] + 1 > Map.MaxIndex) return;
                     Player.CurrentLocation[0]++;
                     break;
                 case "move west":
-                    if (Player.CurrentLocation[0] + 1 > Map.MaxIndex) return;
+                    if (Player.CurrentLocation[0] - 1 < Map.MinIndex) return;
                     Player.CurrentLocation[0]--;
                     break;
                 default:
